b7f1fe8881d1e57237de726936fcbdf5b67b36f6
unknown <amirmm5560@gmail.com>
2023-11-18 10:41:13 -0800

    create routes folder and controllers folder and add The main path of the users in server.js and add express.Router in userRoutes

--- backend/server.js
+++ backend/server.js
@@ -3,5 +3,9 @@ require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.get("/", (req, res) => res.status(200).json({message:"Welcome to the Supprt Desck API"}));
+app.get("/", (req, res) =>
+  res.status(200).json({ message: "Welcome to the Supprt Desck API" })
+);
+app.use("/api/users", require("../routes/userRoutes"));
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
