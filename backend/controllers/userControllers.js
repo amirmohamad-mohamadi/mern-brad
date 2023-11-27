6a45239c2eaa163449af12e3fb6d71c7e66ab2e0
unknown <amirmm5560@gmail.com>
2023-11-27 14:45:54 -0800

    use from bcryptjs and create models and create userSchema for use in registerUser

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -1,15 +1,46 @@
 const expressAsyncHandler = require("express-async-handler");
+const bcrypt = require("bcryptjs");
+const User = require("../models/userModels");
+const { default: mongoose } = require("mongoose");
 
 const registerUser = expressAsyncHandler(async (req, res) => {
-  res.send("Register User");
-});
-
-const loginUser = expressAsyncHandler(async (req, res) => {
-  const { user, password, email } = req.body;
-  if (!user || !password || !email) {
+  const { name, password, email } = req.body;
+  if (!name || !password || !email) {
     res.status(400);
     throw new Error("Plase include all fields");
   }
+
+  // find if user already exists
+
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
+
+  // hash password
+  const salt = await bcrypt.genSalt(10);
+  const hashPassword = await bcrypt.hash(password, salt);
+
+  const user = await User.create({
+    name,
+    email,
+    password: hashPassword,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error("invalid user data");
+  }
+});
+
+const loginUser = expressAsyncHandler(async (req, res) => {
   res.send("Login User");
 });
 
--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -1,6 +1,6 @@
 const errorHandler = (errorHandler, req, res, next) => {
   const statusCode = res.statusCode ?? 500;
-  res.statue(statusCode);
+  res.status(statusCode);
   res.json({
     message: errorHandler.message,
     stack: (process.env.NODE_ENV = "production" ? null : errorHandler.stack),
