285633e3accf624c5ebb4dbb6c6b33b32c80d4ba
unknown <amirmm5560@gmail.com>
2023-12-13 16:27:37 -0800

    create route and controller for get ticket and update ticket and delete ticket

--- backend/controllers/ticketControllers.js
+++ backend/controllers/ticketControllers.js
@@ -17,6 +17,31 @@ const getTickets = asyncHandler(async (req, res, next) => {
   res.status(200).json({ tickets });
 });
 
+// @desc Get user ticket
+// @route Get /api/tickets/:id
+// @access Private
+const getTicket = asyncHandler(async (req, res, next) => {
+  const user = await User.findById(req.user.id);
+
+  if (!user) {
+    throw new Error("user not found");
+  }
+
+  const ticket = await Ticket.findById(req.params.id);
+
+  if (!ticket) {
+    res.status(404);
+    throw new Error("Ticket Not Found!");
+  }
+
+  if (ticket.user.toString() !== req.user.id) {
+    res.status(401);
+    throw new Error("Not Autorized");
+  }
+
+  res.status(200).json({ ticket });
+});
+
 // @desc Create new ticket
 // @route POST /api/tickets
 // @access Private
@@ -42,7 +67,66 @@ const createTicket = asyncHandler(async (req, res, next) => {
   res.status(201).json(ticket);
 });
 
+// @desc Update user ticket
+// @route Put /api/tickets/:id
+// @access Private
+const updateTicket = asyncHandler(async (req, res, next) => {
+  const user = await User.findById(req.user.id);
+
+  if (!user) {
+    throw new Error("user not found");
+  }
+
+  const ticket = await Ticket.findById(req.params.id);
+
+  if (!ticket) {
+    res.status(404);
+    throw new Error("Ticket Not Found!");
+  }
+
+  if (ticket.user.toString() !== req.user.id) {
+    res.status(401);
+    throw new Error("Not Autorized");
+  }
+
+  const updateTicket = await Ticket.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  });
+
+  res.status(200).json({ updateTicket });
+});
+
+// @desc remove user ticket
+// @route Delete /api/tickets/:id
+// @access Private
+const removeTicket = asyncHandler(async (req, res, next) => {
+  const user = await User.findById(req.user.id);
+
+  if (!user) {
+    throw new Error("user not found");
+  }
+
+  const ticket = await Ticket.findById(req.params.id);
+
+  if (!ticket) {
+    res.status(404);
+    throw new Error("Ticket Not Found!");
+  }
+
+  if (ticket.user.toString() !== req.user.id) {
+    res.status(401);
+    throw new Error("Not Autorized");
+  }
+
+  await ticket.deleteOne();
+
+  res.status(200).json({ success: true });
+});
+
 module.exports = {
   getTickets,
+  getTicket,
   createTicket,
+  updateTicket,
+  removeTicket,
 };
--- backend/routes/ticketRoutes.js
+++ backend/routes/ticketRoutes.js
@@ -2,11 +2,19 @@ const express = require("express");
 const protect = require("../middleware/authMiddleware");
 const {
   getTickets,
+  getTicket,
   createTicket,
+  updateTicket,
+  removeTicket,
 } = require("../controllers/ticketControllers");
 
 const router = express.Router();
 
 router.route("/").get(protect, getTickets).post(protect, createTicket);
+router
+  .route("/:id")
+  .get(protect, getTicket)
+  .put(protect, updateTicket)
+  .delete(protect, removeTicket);
 
 module.exports = router;
